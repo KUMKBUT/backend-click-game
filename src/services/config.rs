ab4c9b2924f436103cd39539d603ad77914e9139
KUMKBUT <leha.schmelev@mail.ru>
2026-04-26 23:44:12 +0500

    -added get info service

--- src/services/config.rs
+++ src/services/config.rs
@@ -1,10 +1,18 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum ServiceHistoryStatus {
+    #[serde(rename = "to_service")]
+    ToService,
+    #[serde(rename = "from_service")]
+    FromService,
+}
+
 #[derive(Serialize, Deserialize)] 
 pub struct ServiceHistoryItem {
 	pub id: i64,
 	pub uuid: String,
-    pub status: String,
+    pub status: ServiceHistoryStatus,
 	pub ammount: i64,
 	pub date: i64
 }
@@ -14,6 +22,7 @@ pub struct ServiceCreateRes {
 	pub first_name: String,
 	pub description: String,
 	pub balance: i64,
+	pub callback_url: String,
 	pub history: Vec<ServiceHistoryItem>,
 	pub url_img: String,
 	pub reg_date: i64,
@@ -22,4 +31,15 @@ pub struct ServiceCreateRes {
 pub struct ServiceCreateReq {
 	pub name: String,
 	pub url_img: String,
+	pub callback_url: String,
+}
+#[derive(Serialize, Deserialize, sqlx::FromRow, Debug, Clone)]
+pub struct ServiceGetInfoRes {
+	pub uuid: String,
+	pub first_name: String,
+	pub description: String,
+	pub balance: i64,
+	pub callback_url: String,
+	pub url_img: String,
+	pub reg_date: i64,
 }
--- src/services/handlers.rs
+++ src/services/handlers.rs
@@ -1,11 +1,11 @@
-use axum::{Json, http::{HeaderMap, StatusCode, header}, extract::{State}};
-use redis::{aio::ConnectionManager, AsyncCommands};
+use axum::{Json, http::{StatusCode}};
+use redis::{AsyncCommands};
 use std::{time::{SystemTime, UNIX_EPOCH}};
-use sqlx::postgres::{PgPool};
+use sqlx;
 
 use crate::{SharedState};
 use super::{ 
-    config::{ServiceCreateReq, ServiceCreateRes}
+    config::{ServiceCreateReq, ServiceCreateRes, ServiceGetInfoRes}
 };
 use crate::{helpers::get_game_user};
 
@@ -24,6 +24,7 @@ pub async fn process_create_service(
     let ServiceCreateReq {
         name,
         url_img,
+        callback_url
     } = payload;
 
 	let now = SystemTime::now()
@@ -34,36 +35,73 @@ pub async fn process_create_service(
 	let uuid = format!("svc_{}_{}", creator_id.id, now);
 
     sqlx::query(
-        "INSERT INTO service (creator_id, name, url_img, balance, reg_date)
+        "INSERT INTO service (creator_id, name, url_img, callback_url, reg_date)
          VALUES ($1, $2, $3, $4, $5)"
     )
     .bind(creator_id.id)
     .bind(&name)
     .bind(&url_img)
-    .bind(0_i64)
+    .bind(&callback_url)
     .bind(now)
     .execute(&state.db)
     .await
     .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    let _: () = redis
-        .set_ex(
-            format!("service:{}", uuid),
-            name.clone(),
-            3600,
-        )
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
     let response = ServiceCreateRes {
-        uuid,
+        uuid: uuid.clone(),
         first_name: name,
         description: "".to_string(),
         balance: 0,
+        callback_url: callback_url,
         history: vec![],
         url_img,
         reg_date: chrono::Utc::now().timestamp(),
     };
 
+    let json_data = serde_json::to_string(&response)
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    let _: () = redis
+        .set_ex(
+            format!("service:{}", uuid),
+            json_data,
+            3600,
+        )
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    
+
     Ok(Json(response))
+}
+
+pub async fn process_get_info_service(
+    state: &SharedState,
+    id: &str,
+) -> Result<Json<ServiceGetInfoRes>, (StatusCode, String)>{
+    let mut redis = state.redis.clone();
+
+    let key = format!("service: {}", id);
+
+    if let Ok(Some(cached_data)) = redis.get::<_, Option<String>>(&key).await {
+        if let Ok(response) = serde_json::from_str::<ServiceGetInfoRes>(&cached_data) {
+            return Ok(Json(response));
+        }
+    };
+
+    let service = sqlx::query_as::<_, ServiceGetInfoRes>(
+        "SELECT uuid, name as first_name, description, balance, callback_url, url_img, reg_date
+        FROM services WHERE uuid + $1"
+    )
+    .bind(&id)
+    .fetch_optional(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Ошибка БД: {}", e)))?
+    .ok_or((StatusCode::NOT_FOUND, "Сервис не найден".into()))?;
+
+    if let Ok(json) = serde_json::to_string(&service) {
+        let _: () = redis.set_ex(&key, json, 3600).await.unwrap_or_default();
+    }
+
+    Ok(Json(service))
 }
